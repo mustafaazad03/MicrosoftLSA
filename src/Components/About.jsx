0bbefa4265dfbd37067604d7039b1e3b99f0900d
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-12-08 22:38:17 +0530

    Added portfolio for azure

---
+++ src/Components/About.jsx
@@ -0,0 +1,100 @@
+/**
+ * About component
+ *
+ * Space for you to describe more about yourself.
+ */
+
+import React from "react";
+
+/**
+ * About background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that
+ * represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a image you
+ * freely use on your site.
+ */
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description =
+  "I'm a MERN Stack developer and Blockchain developer. Work as a frontend and backend developer. I have experience in React, Redux, Node, Express, MongoDB, Solidity, and Web3.js.";
+
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
+const skillsList = [
+  "React.js",
+  "Redux",
+  "Node.js",
+  "Express.js",
+  "MongoDB",
+  "Solidity",
+  "Web3.js",
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "Bootstrap",
+  "Material UI",
+  "Git",
+  "GitHub",
+  "Heroku",
+  "Netlify",
+  "Firebase",
+  "Python",
+  "C++",
+  "C",
+];
+
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote =
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my mern stack and blockchain experience I continually look for new and better ways to make tech accessible by all.";
+
+const About = () => {
+  return (
+    <section className="padding" id="about">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>About Me</h2>
+        <p className="large">{description}</p>
+        <hr />
+        <ul
+          style={{
+            textAlign: "left",
+            columns: 2,
+            fontSize: "1.25rem",
+            margin: "2rem 3rem",
+            gap: "3rem",
+          }}
+        >
+          {skillsList.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
